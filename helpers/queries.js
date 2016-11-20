ffb624cde0381a70a227b52af47c38da236b916c
Jack Zeiders <zeiders@usc.edu>
2016-11-19 17:44:43 -0800

    Added Vessel

--- helpers/queries.js
+++ helpers/queries.js
@@ -24,49 +24,90 @@ var disconnect = function() {
 		return true;
 	});
 };
-var jobQueryConstructor = function(arrival_time, container_size, container_id, port, terminal, destination){
-  var cols = "INSERT INTO jobs (arrival_time, container_size, container_id, port, terminal, destination)";
-  var vals = " VALUES ('"+arrival_time+"',"+container_size+",'"+container_id+"','"+port+"','"+ terminal + "','"+destination+"');";
-  console.log(cols+vals);
-  return cols+vals;
+var jobQueryConstructor = function(arrival_time, container_size, container_id, port, terminal, destination) {
+	var cols = "INSERT INTO jobs (arrival_time, container_size, container_id, port, terminal, destination)";
+	var vals = " VALUES ('" + arrival_time + "'," + container_size + ",'" + container_id + "','" + port + "','" + terminal + "','" + destination + "');";
+	console.log(cols + vals);
+	return cols + vals;
 };
 
 var generateJobsFromArrivals = function() {
-  var blank_job = {
-    arrival_date: null,
-    price: null,
-    container_size: null,
-    container_id: null,
-    source: null,
-    desination: null,
-    trucker_id: null,
-    is_finished: null
-  };
-  var handler = function(err,data){
-    console.log(err)
-  };
+	var blank_job = {
+		arrival_date: null,
+		price: null,
+		container_size: null,
+		container_id: null,
+		source: null,
+		desination: null,
+		trucker_id: null,
+		is_finished: null
+	};
+
 
 	client.query("SELECT * FROM arrival WHERE mode_of_exit='Truck'", function(err, data) {
 		if (err) throw err;
 		console.log("data");
-    for(var i = 0; i < data.rows.length;i++){
-      var item = data.rows[i];
-      var query = jobQueryConstructor(item.est_arrival,item.container_size, item.container_num,item.unload_port, item.arriving_term,item.inland_point);
-      client.query(query, handler);
-    }
-		console.log(data.rows[0]);
+		for (var i = 0; i < sizes.length; i++) {
+			var item = sizes[i];
+			var query = jobQueryConstructor(item.est_arrival, item.container_size, item.container_num, item.unload_port, item.arriving_term, item.inland_point);
+			client.query(query, handler);
+		}
+		console.log(sizes[0]);
 	});
 };
-var getChasisCount = function(){
-  client.query("SELECT * FROM chasis WHERE in_use=false", function(err, data){
-    return data.length;
-  });
+var handler = function(err, data) {
+	console.log(err);
 };
-
+var chassisGen = function() {
+	var sizes = [20, 40, 45];
+	for (var i = 0; i < 2500; i++) {
+		size = sizes[Math.floor(Math.random() * 3)];
+		var query = "INSERT INTO chassis (size, is_checked_out) VALUES (" + size + ",false);"
+		console.log(query);
+		client.query(query, handler);
+	}
+};
+var getChassisCount = function() {
+	client.query("SELECT * FROM chasis WHERE in_use=false", function(err, data) {
+		return data.length;
+	});
+};
+var assignChassis = function() {
+	client.query("");
+};
+var vesselQuery = function(vessel) {
+	return new Promise(function(res, rej) {
+    var counts = [0,0,0]
+    var query = 'SELECT container_size FROM arrival WHERE vessel_name=$$' + vessel + '$$';
+    console.log(query);
+		client.query(query, function(err, data) {
+			if (err) rej(err)
+      console.log(vessel);
+      console.log(data);
+      var sizes = data.rows.map(function(v){
+        return v.container_size
+      })
+      for(var i = 0; i < sizes.length;i++){
+        console.log(sizes[i])
+        if(sizes[i] == 20)
+          counts[0]+=1;
+        if(sizes[i] == '40')
+          counts[1]+=1;
+        if(sizes[i] == '45')
+          counts[2]+=1;
+      }
+      res(counts);
+		});
+	});
+};
 var client = new pg.Client(pgConfig);
 var queries = {
 	connect: connect, // Attempts to connect to DB;
-	generateJobsFromArrivals: generateJobsFromArrivals // Generate all jobs from arrival_data
+	generateJobsFromArrivals: generateJobsFromArrivals, // Generate all jobs from arrival_data
+	chassisGen: chassisGen,
+	getChassisCount: getChassisCount,
+	assignChassis: assignChassis,
+	vesselQuery: vesselQuery
 };
 
 
--- server.js
+++ server.js
@@ -1,16 +1,28 @@
 var express = require("express");
 var queries = require("./helpers/queries.js");
-
+var bodyParser = require('body-parser');
 var app = express();
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
 
 app.get("/", function() {
 
+});
+app.post("/vessel", function(req,res){
+  var vessel = req.body.vessel;
+  console.log(vessel + "Vessel");
+  queries.vesselQuery(vessel).then(function(data){
+    res.send(data);
+  });
 });
 
 app.listen(3000, function() {
 	console.log("Started Server");
 	queries.connect().then(function(data){
-		queries.generateJobsFromArrivals();
+    console.log("Connected");
   }
 ).catch(function(err){
   console.log(err);
